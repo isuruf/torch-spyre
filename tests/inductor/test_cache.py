0d5fcb2761acb3601fa4be73a9ca03c07fcf849e
Isuru Fernando <ifernando@openteams.com>
2026-04-03 20:36:00 +0000

    Enable FX Graph caching
    Making _{pre/post}_fusion_custom_pass derive from CustomGraphPass
    makes things cacheable.

---
+++ tests/inductor/test_cache.py
@@ -0,0 +1,37 @@
+# Copyright 2026 The Torch-Spyre Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+import torch
+from torch._inductor.utils import fresh_cache, clear_caches
+from torch._dynamo.utils import counters
+
+
+class TestCache(unittest.TestCase):
+    def test_cache(self):
+        with fresh_cache():
+            x = torch.randn((64, 64)).to("spyre")
+            cfn = torch.compile(torch.abs, dynamic=False)
+
+            cfn(x)
+            artifacts = torch.compiler.save_cache_artifacts()
+            self.assertIsNotNone(artifacts)
+            self.assertEqual(counters["inductor"]["fxgraph_cache_miss"], 1)
+
+            artifact_bytes, cache_info = artifacts
+            torch._dynamo.reset()
+            clear_caches()
+
+            cfn(x)
+            self.assertEqual(counters["inductor"]["fxgraph_cache_hit"], 1)
